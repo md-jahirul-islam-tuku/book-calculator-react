04b01b7c3efd60d4dcde430d84087bd888fef936
Tuku <tukuwebian@gmail.com>
2025-11-27 12:09:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./assets/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+        <Navbar></Navbar>
+      <main>
+        
+      </main>
+    </>
+  );
+}
+
+export default App;
